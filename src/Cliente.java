45344e950e867489f66722ce39d2e392c28748c5
alvarofgomes <alvarogomes098@gmail.com>
2025-11-07 09:14:11 -0300

    primeiro commit do projeto tartaruga cometa

---
+++ src/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+
+	private String nome;
+	private String cpfCnpj;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+	
+	
+}
---
+++ src/Produto.java
@@ -0,0 +1,31 @@
+public class Produto {
+
+	private String nomeDoProduto;
+	private double peso;
+	private double valor;
+	
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+	
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+	
+	public double getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+}
