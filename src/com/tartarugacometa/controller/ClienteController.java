5b01a7b447e6ea1dc327d6f4eb8d1aeaddaeed93
alvarofgomes <alvarogomes098@gmail.com>
2025-11-14 13:54:14 -0300

    adicionando regra para identificar pessoa fisica e juridica

--- src/com/tartarugacometa/controller/ClienteController.java
+++ src/com/tartarugacometa/controller/ClienteController.java
@@ -9,22 +9,18 @@ public class ClienteController {
 	Scanner sc = new Scanner(System.in);
 	
 	private Cliente cliente;
+	char l = verificarPessoaFisicaOuJuridica();
 	
 	public Cliente cadastrarCliente() {
-		
-		System.out.println("O cliente e pessoa física ou juridica(f/j)?");
-		char l = sc.next().charAt(0);
-		
-		if(l == 'f') {
+
+		if(this.l == 'f') {
 			System.out.println("Digite o nome do cliente: ");
-			sc.nextLine(); 
 			String n = sc.nextLine();
 			System.out.println("Digite o CPF do cliente: ");
 			String c = sc.nextLine();
 			this.cliente = new Cliente(n,c);
-		}else if(l == 'j') {
+		}else if(this.l == 'j') {
 			System.out.println("Digite o nome do cliente: ");
-			sc.nextLine(); 
 			String n = sc.nextLine();
 			System.out.println("Digite o CNPJ do cliente: ");
 			String c = sc.nextLine();
@@ -36,22 +32,38 @@ public class ClienteController {
 		
 	}
 	
-	public void carregarCliente() {
+	public void atualizarDadosDoCliente() {
 		
 	}
 	
-	public void editarCliente() {
-		
-	}
-	
-	public void excluirCliente() {
+	public void excluirContaDoCliente() {
 		
 	}
 	
 	public void exibirInfo() {
-		System.out.println("Dados DO CLIENTE: ");
-		System.out.printf("Nome: %s\n", this.cliente.getNome());
-		System.out.printf("CPF: %s\n", this.cliente.getCpfCnpj());
+		
+		if(this.l == 'f') {
+			System.out.println("Dados DO CLIENTE: ");
+			System.out.printf("Nome: %s\n", this.cliente.getNome());
+			System.out.printf("CPF: %s\n", this.cliente.getCpfCnpj());
+		}else if(this.l == 'j') {
+			System.out.println("Dados DO CLIENTE: ");
+			System.out.printf("Nome: %s\n", this.cliente.getNome());
+			System.out.printf("CNPJ: %s\n", this.cliente.getCpfCnpj());
+		}else {
+			System.out.println("Erro. ");
+		}
+		
+	}
+	
+	public static char verificarPessoaFisicaOuJuridica() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("O cliente e pessoa física ou juridica(f/j)?");
+		char l = sc.next().charAt(0);
+		
+		return l;
 	}
 	
 }
